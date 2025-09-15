e338537b921b1f3ae9614626bee694a734d3c7e7
Siraj Mohammed <bstech500@gmail.com>
2025-09-16 00:03:38 +0300

    final commit

--- server/src/app.js
+++ server/src/app.js
@@ -15,7 +15,7 @@ const app = express();
 
 // Middleware
 app.use(cors( {
-  origin: 'http://localhost:5173',
+  origin: process.env.CLIENT_URL,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
