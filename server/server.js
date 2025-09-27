4eb9673ef9871f794bbce00d3a9144b477922a30
Siraj Mohammed <bstech500@gmail.com>
2025-09-27 20:47:39 +0300

    initial commit

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const app = require('./src/app');
 const { setupSocket } = require('./src/sockets/socket');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 const server = http.createServer(app);
 
